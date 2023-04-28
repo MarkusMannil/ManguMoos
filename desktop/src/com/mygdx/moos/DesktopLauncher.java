c3ba793e151a3eb83c0f9e089b431f512381a7aa
MarkusMannil <111markus111@gmail.com>
2023-04-28 18:37:22 +0300

    Initial commit

---
+++ desktop/src/com/mygdx/moos/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.moos;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.moos.GeimClass;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("ManguMoos");
+		new Lwjgl3Application(new GeimClass(), config);
+	}
+}
